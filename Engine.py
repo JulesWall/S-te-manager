e87cb96eb034d3d9b09db6b0e4f08ec945baba16
JulesWall <63977848+JulesWall@users.noreply.github.com>
2021-04-19 18:48:24 +0200

    add player verification in Engine

--- Engine.py
+++ Engine.py
@@ -3,6 +3,7 @@ import asyncio
 
 from config import *
 from commands import *
+from classes.checkers import *
 
 class Engine():
 
@@ -20,6 +21,8 @@ class Engine():
         if has_prefix:
             if is_maintenance and self.message.author.id not in MAINTENANCE_AUTHORIZE:return "break"
             elif self.message.guild.id in SERVER_WHITELISTED:
+                if not self.message.content.startswith('!pr') and not Checkers(self.message.author.id).player():
+                    await self.message.channel.send('Fais !profil pour t\'enregistrer');return "break"
                 try:
                     command_info = commands.get(self.message.content[1:].split()[0])
                     command = command_info[0]
