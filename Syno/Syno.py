2402e16c8fd68c3deb2fcf369469eb6ff7a09ae5
JulesWall <63977848+JulesWall@users.noreply.github.com>
2021-04-20 16:02:20 +0200

    fix vehicule's statuts determination

--- Syno/Syno.py
+++ Syno/Syno.py
@@ -41,15 +41,18 @@ class Syno():
             syno_vhl_inverted = dict(syno_vhl_inverted)
 
             result = 0
+            print(vhl.vehicule)
+            print(syno_vhl)
+            print(vhl_required)
             for i in syno_vhl_inverted.keys():
                 result = syno_vhl[i] - (vhl_required[i] - result)
+                
+                print(result)
                 if result < 0:
-                    vhl.statut = 0
                     break
-                else:
-                    continue
-            vhl.statut = 1
-
+            if result >= 0 : vhl.statut = 1
+            else:
+                vhl.statut = 0
         else:
             vhl.statut = 0
         
