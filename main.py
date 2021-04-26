3e5219f150b7985e961e9476c5f35294f03ea463
JulesWall <63977848+JulesWall@users.noreply.github.com>
2021-04-26 18:50:39 +0200

    First implement of messages logs

--- main.py
+++ main.py
@@ -18,8 +18,22 @@ async def on_ready():
     delete_expired_wh()
 
 @client.event
-async def on_message_edit(before, after): await on_message(after)   
-    
+async def on_message_edit(before, after): 
+    await on_message(after)
+    log = client.get_guild(705059899750613013).get_channel(836277984486621194)
+    embed=discord.Embed(color=0xff6f00)
+    string = f":wrench: __**Info**__ \n**User** : {after.author.id}, <@{after.author.id}>\n **Channel** : <#{after.channel.id}>\n\n :page_facing_up:  **Message avant**:```{before.content}```\n :page_with_curl:  **Message après**:```{after.content}```"
+    embed.add_field(name="Message édité", value=string, inline=False)
+    await log.send(embed=embed)  
+
+@client.event
+async def on_message_delete(message):
+    log = client.get_guild(705059899750613013).get_channel(836277984486621194)
+    embed=discord.Embed(color=0xff6f00)
+    string = f" :microscope:  __**Info**__ \n**User** : {message.author.id}, <@{message.author.id}>\n **Channel** : <#{message.channel.id}>\n\n :page_facing_up:  **Message supprimé**:```{message.content}```"
+    embed.add_field(name="Message supprimé", value=string, inline=False)
+    await log.send(embed=embed)
+
 @client.event
 async def on_message(message): await Engine(message,  client).run()
 
