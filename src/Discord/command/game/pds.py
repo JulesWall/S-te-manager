56fd3f6f4aed6fbc8be536b8eeee2553c37642f1
JulesWall <juels.yeah@gmail.com>
2021-05-15 19:51:15 +0300

    fixes

--- src/Discord/command/game/pds.py
+++ src/Discord/command/game/pds.py
@@ -1,4 +1,4 @@
-import discord 
+import discord
 from db.function.top import get_top
 from config import *
 
@@ -13,9 +13,9 @@ class Pds(GameCommand):
 
     async def run(self):
         if not self.has_permission : return await self.not_permission()
-        
+
         profil = ExistProfil(self.message.author.id)
-        if Checkers(self.message.author.id).is_astreinte(): 
+        if Checkers(self.message.author.id).is_astreinte():
             time_service = ((__import__("time").time() - profil.end_service())/60)
             await self.message.channel.send(f"Fin de service pour <@{self.message.author.id}>")
             salaire = 0 # self.player.update_money((round(((1.5**1.1)*time_service-1.56), 2)))
@@ -25,11 +25,10 @@ class Pds(GameCommand):
             profil.add_service_time(time_service)
             await self.message.channel.send(f"<@{self.message.author.id}> - Pendant votre service, vous avez gagné {salaire} € et perdus {CP*-1} % de condition physique.")
         else:
-            if len(self.message.content.split()) > 1: 
-                if Checkers(self.message.author.id).cta() and self.message.content.split()[1]=="cta": cta=1
+            if len(self.message.content.split()) > 1:
+                if Checkers(self.message.author.id).is_cta() and self.message.content.split()[1]=="cta": cta=1
                 else: cta=0
             else: cta=0
             profil.start_service(cta)
             if cta: await self.message.channel.send(f"Début de service au cta pour <@{self.message.author.id}>")
             else: await self.message.channel.send(f"Début de service pour <@{self.message.author.id}>")
-
