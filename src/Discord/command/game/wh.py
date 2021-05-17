b02a81c932e715fbb6f455c73f9c22b6fa14c1a3
JulesWall <juels.yeah@gmail.com>
2021-05-17 22:01:54 +0300

    wh list's minor modifications

--- src/Discord/command/game/wh.py
+++ src/Discord/command/game/wh.py
@@ -24,15 +24,15 @@ class wh(CtaCommand):
         if not self.has_permission : return await self.not_permission()
 
         try:
-            arg = await self.get_args(self.args1, 1) 
+            arg = await self.get_args(self.args1, 1)
             assert arg != None
-        except: 
+        except:
             await self.error()
-            return None        
+            return None
         await arg()
-    
+
     async def send(self):
-        try: 
+        try:
             alias = self.message.content.split(" ")[2]
             wh = ExistWh(alias)
         except : return await self.error()
@@ -50,15 +50,15 @@ class wh(CtaCommand):
             alias, name, link = ' '.join(self.message.content.split(" ")[2:]).split("|")
             WhInit(alias, name, link)
             await self.message.channel.send(f"Le webhook `{name}` avec l'url `{link}` et l'alias {alias} a bien été ajouté à la base de donnée. Il sera automatiquement supprimé si il n'est pas utilisé pendant plus de 30 jours. Vous pouvez le supprimer en le remplacant avec un autre webhook avec le même alias.")
-        except Exception as e: 
+        except Exception as e:
             await self.message.channel.send(e)
             return await self.error()
-    
+
     async def listt(self):
-        
+
         datas = Querry("SELECT alias FROM wh ORDER BY lastuse DESC")
         content = ""
-        maxpages = math.ceil(len(datas)/40)
+        maxpages = math.ceil(len(datas)/25)
 
         try:
             page = int(self.message.content.split(" ")[2])
@@ -67,15 +67,13 @@ class wh(CtaCommand):
         except:
             page = 1
 
-        for i in range(40*(page-1), 40*(page)):
+        for i in range(25*(page-1), 25*(page)):
             try : idwh = datas[i][0]
             except : break
-            wh = ExistWh(idwh) 
+            wh = ExistWh(idwh)
             content += f"\n **-** {wh.alias} > {wh.name}"
 
         content += f"\n\n Page `{page}`/`{maxpages}` | Do !wh list <p>"
-        embed=discord.Embed(title="Wh list", description=f"{content}", color=0x00ffe1)
+        embed=discord.Embed(title="Liste des alias de WebHook (alias > nom)", description=f"{content}", color=0x00ffe1)
         embed.set_footer(text=FOOTER)
         await self.message.channel.send(embed=embed)
-
-    
