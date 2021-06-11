4c41de67ff2fd2bc4d13cebdd1ef8130c2d0cfa7
JulesWall <63977848+JulesWall@users.noreply.github.com>
2021-06-11 20:03:56 +0200

    Update loop.py

--- src/Discord/loop/loop.py
+++ src/Discord/loop/loop.py
@@ -20,5 +20,16 @@ class RLoop():
             chan_to_delete = delete_expired_house()
             for c in chan_to_delete:
                 await self.bot.get_guild(705059899750613013).get_channel(c).delete()
-
+                
+            
+            game = discord.Game("📵 CTA indisponible !")
+            status=discord.Status.idle
+            service = Querry("SELECT * FROM service")     
+            for data in service:
+               cta = data[4]
+               if cta: game = 
+                    discord.Game("📱 CTA disponible !")
+                    status=discord.Status.online
+            await self.bot.change_presence(status=status, activity=game)
+            
             await asyncio.sleep(200)
