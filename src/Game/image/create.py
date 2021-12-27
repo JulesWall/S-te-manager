5a394b858c4a40ee6fba911703f60b585bf25e95
JulesWall <63977848+JulesWall@users.noreply.github.com>
2021-12-27 11:01:41 +0100

    Update create.py

--- src/Game/image/create.py
+++ src/Game/image/create.py
@@ -59,8 +59,8 @@ class Bipimage():
         self.draw = ImageDraw.Draw(self.im)
         
     def save(self):
-        self.im.save(self.path + f"/Bip/BIP_{self.playerid}.png", "PNG")
-        return str (self.path + f"/Bip/BIP_{self.playerid}.png")
+        self.im.save(self.path + f"Bip/BIP_{self.playerid}.png", "PNG")
+        return str (self.path + f"Bip/BIP_{self.playerid}.png")
 
     def on(self, name, statut):
         draw = ImageDraw.Draw(self.im)
@@ -68,8 +68,8 @@ class Bipimage():
         draw.text((850, 200), "CSP Sete", font=self.font40)
         draw.text((340, 280), f"{name}", font=self.font100)
         draw.text((340, 480), f"{statut}", font=self.font100)
-        self.im.save(self.path + f"/Bip/BIP_{self.playerid}.png", "PNG")
-        return str (self.path + f"/Bip/BIP_{self.playerid}.png")
+        self.im.save(self.path + f"Bip/BIP_{self.playerid}.png", "PNG")
+        return str (self.path + f"Bip/BIP_{self.playerid}.png")
 
     def alert(self, name, vhl, poste, motif):
         self.draw.text((340, 280), f"{name}", font=self.font100)
