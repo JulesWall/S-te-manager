42e7ab8b5238b414ae3691ebdf446bc6b2c91da7
JulesWall <63977848+JulesWall@users.noreply.github.com>
2021-12-27 12:29:16 +0100

    bip part 2

--- src/Discord/command/game/bip.py
+++ src/Discord/command/game/bip.py
@@ -1,4 +1,5 @@
 import discord
+import discord.ui as dui
 from config import *
 
 from Discord.command.Command import *
@@ -12,18 +13,92 @@ class Bip(GameCommand):
     def __init__(self, message, bot):
         GameCommand.__init__(self, message, bot)
         self.pid = self.message.author.id
+        self.args1 = {
+            "on":self.on,
+            "off":self.off
+        }
 
     async def run(self):
         if not self.has_permission : return await self.not_permission()
 
-        profil = ExistProfil(self.pid)
+        self.profil = ExistProfil(self.pid)
         if not Checkers(self.pid).own_pager(): BipInit(self.pid, "OFF")
 
-        bip = ExistBip(self.pid)
-        if bip.statut == "OFF" : path = Bipimage("OFF", self.pid).save()
-        else : path = Bipimage("ON", self.pid).on(profil.name, bip.statut).save()
-        self.file = discord.File(str(path))
-        await self.message.channel.send(file=self.file)
+        self.bip = ExistBip(self.pid)
+        if len(self.message.content.split()) == 1:
+            if bip.statut == "OFF" : await self.off(f"**Décroche le bip de sa ceinture et le regarde**")
+            else : await self.on(f"**Décroche le bip de sa ceinture et appuis sur le bouton pour en allumer l'écran**")
+        else :
+            try:
+                arg = await self.get_args(self.args1, 1) 
+                assert arg != None
+            except: 
+                await self.error()
+                return None        
+            await arg()
+        
+        #await self.message.channel.send(file=self.file)
+    
+    async def on(self, msg=f"**Allume son bip**"):
+        self.bip.update("Disponible")
+        await MessageSender(self.message, self.bot).wh(
+                name = self.message.author.display_name,
+                avatar_url=self.message.author.avatar_url,
+                msg = msg
+            )
+        self.path = Bipimage("ON", self.pid).on(self.profil.name, self.bip.statut).save()
+        self.file = discord.File(str(self.path))
 
+        self.view = add_items([
+            dui.Button(style=discord.ButtonStyle.red, label="OFF", emoji="<:bip:924613243496960030>"),
+            dui.Button(style=discord.ButtonStyle.grey, label="     "),
+            dui.Button(style=discord.ButtonStyle.red, label="SOS", emoji="🆘")
+        ], [
+            self.roff,
+            self.rnothing,
+            self.rnothing,
+        ], dui.View()
+        )
 
+        await self.message.channel.send(file=self.file, view=self.view)
 
+    async def off(self, msg=f"**Eteins son bip**"):
+        self.bip.update("OFF")
+        await MessageSender(self.message, self.bot).wh(
+                name = self.message.author.display_name,
+                avatar_url=self.message.author.avatar_url,
+                msg = msg
+            )
+        self.path = Bipimage("OFF", self.pid).save()
+        self.file = discord.File(str(self.path))
+        self.view = add_items([
+            dui.Button(style=discord.ButtonStyle.green, label="ON", emoji="<:bip:924613243496960030>"),
+            dui.Button(style=discord.ButtonStyle.grey, label="     "),
+            dui.Button(style=discord.ButtonStyle.grey, label="     ")
+        ], [
+            self.ron,
+            self.rnothing,
+            self.rnothing,
+        ], dui.View())
+
+        await self.message.channel.send(file=self.file, view=self.view)
+
+    async def rnothing(self, interaction):pass
+    
+    async def ron(self, interaction):
+        async with self.message.channel.typing():
+           await interaction.message.delete()
+           await self.on()
+
+    async def roff(self, interaction):
+        async with self.message.channel.typing():
+           await interaction.message.delete()
+           await self.off()
+
+def add_items(buttons, r, v):
+    i = 0
+    for b in buttons: 
+        b.callback = r[i]
+        v.add_item(b)
+        i += 1
+    return v
