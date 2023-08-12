4c22ed3482a395e36b13ddfac0522206fb1cb132
JulesWall <63977848+JulesWall@users.noreply.github.com>
2023-08-12 23:58:43 +0200

    Nouvelle données vhl

--- csp.sql
+++ csp.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1deb1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : lun. 07 août 2023 à 10:40
--- Version du serveur : 8.0.33-0ubuntu0.23.04.2
--- Version de PHP : 8.1.12-1ubuntu4
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  sam. 12 août 2023 à 21:56
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `csp`
+-- Base de données :  `csp2`
 --
 
 -- --------------------------------------------------------
@@ -27,25 +28,29 @@ SET time_zone = "+00:00";
 -- Structure de la table `bip`
 --
 
-CREATE TABLE `bip` (
-  `id` mediumint NOT NULL,
-  `id_owner` bigint NOT NULL,
-  `expiration` bigint NOT NULL,
-  `has_low_battery` int NOT NULL
+DROP TABLE IF EXISTS `bip`;
+CREATE TABLE IF NOT EXISTS `bip` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `id_owner` bigint(20) NOT NULL,
+  `expiration` bigint(20) NOT NULL,
+  `has_low_battery` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `House`
+-- Structure de la table `house`
 --
 
-CREATE TABLE `House` (
-  `id` int NOT NULL,
-  `owner_id` bigint NOT NULL,
-  `last_message` bigint NOT NULL,
-  `chan_id` bigint NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+DROP TABLE IF EXISTS `house`;
+CREATE TABLE IF NOT EXISTS `house` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` bigint(20) NOT NULL,
+  `last_message` bigint(20) NOT NULL,
+  `chan_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -53,18 +58,20 @@ CREATE TABLE `House` (
 -- Structure de la table `profil`
 --
 
-CREATE TABLE `profil` (
-  `id` mediumint NOT NULL,
-  `idd` bigint NOT NULL,
-  `name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `grade` int NOT NULL,
-  `hierarchie` int NOT NULL,
-  `poste` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `money` int NOT NULL,
-  `CP` int NOT NULL,
-  `location` bigint NOT NULL,
-  `service_time` bigint NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+DROP TABLE IF EXISTS `profil`;
+CREATE TABLE IF NOT EXISTS `profil` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `idd` bigint(20) NOT NULL,
+  `name` longtext COLLATE utf8mb4_bin NOT NULL,
+  `grade` int(11) NOT NULL,
+  `hierarchie` int(11) NOT NULL,
+  `poste` text COLLATE utf8mb4_bin NOT NULL,
+  `money` int(11) NOT NULL,
+  `CP` int(11) NOT NULL,
+  `location` bigint(20) NOT NULL,
+  `service_time` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -72,13 +79,15 @@ CREATE TABLE `profil` (
 -- Structure de la table `service`
 --
 
-CREATE TABLE `service` (
-  `id` int NOT NULL,
-  `idd` bigint NOT NULL,
-  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `time` bigint NOT NULL,
-  `cta` int NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+DROP TABLE IF EXISTS `service`;
+CREATE TABLE IF NOT EXISTS `service` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idd` bigint(20) NOT NULL,
+  `name` text COLLATE utf8mb4_bin NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `cta` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -86,12 +95,14 @@ CREATE TABLE `service` (
 -- Structure de la table `tph`
 --
 
-CREATE TABLE `tph` (
-  `id` mediumint NOT NULL,
-  `id_owner` bigint NOT NULL,
-  `expiration` bigint NOT NULL,
-  `Frequency` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+DROP TABLE IF EXISTS `tph`;
+CREATE TABLE IF NOT EXISTS `tph` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `id_owner` bigint(20) NOT NULL,
+  `expiration` bigint(20) NOT NULL,
+  `Frequency` text COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=180 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -99,11 +110,13 @@ CREATE TABLE `tph` (
 -- Structure de la table `tph_frequency`
 --
 
-CREATE TABLE `tph_frequency` (
-  `id` mediumint NOT NULL,
-  `Frequency` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `channels` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+DROP TABLE IF EXISTS `tph_frequency`;
+CREATE TABLE IF NOT EXISTS `tph_frequency` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `Frequency` text COLLATE utf8mb4_bin NOT NULL,
+  `channels` longtext COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Déchargement des données de la table `tph_frequency`
@@ -123,33 +136,48 @@ INSERT INTO `tph_frequency` (`id`, `Frequency`, `channels`) VALUES
 -- Structure de la table `vhl`
 --
 
-CREATE TABLE `vhl` (
-  `id` int NOT NULL,
-  `statut` int NOT NULL,
-  `véhicule` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `cord` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `syno` int NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+DROP TABLE IF EXISTS `vhl`;
+CREATE TABLE IF NOT EXISTS `vhl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `statut` int(11) NOT NULL,
+  `véhicule` longtext COLLATE utf8mb4_bin NOT NULL,
+  `cord` text COLLATE utf8mb4_bin NOT NULL,
+  `syno` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Déchargement des données de la table `vhl`
 --
 
 INSERT INTO `vhl` (`id`, `statut`, `véhicule`, `cord`, `syno`) VALUES
-(1, 0, 'CCFM-601', '(32,275)', 1),
-(2, 0, 'CCFM-602', '(508, 275)', 1),
-(3, 0, 'CCGC-301', '(269,275)', 1),
-(4, 0, 'EPA-001', '(33,186)', 1),
-(5, 0, 'FPTL-121', '(269,186)', 1),
-(6, 0, 'FPTSR-181', '(507,186)', 1),
-(7, 0, 'VLM-131', '(258,61)', 2),
-(8, 0, 'VLHR-104', '(259,169)', 2),
-(9, 0, 'PCC-34-1', '(498,169)', 2),
-(10, 0, 'VSAV-221', '(268,95)', 1),
-(11, 0, 'VSAV-222', '(508,96)', 1),
-(12, 0, 'VTU-TP-161', '(32,95)\r\n', 1),
-(13, 0, 'VL-503', '(35,169)', 2),
-(14, 0, 'VPMA-34-3', '(258,242)', 2);
+(30, 0, 'VLCG-151', '(20,864)', 1),
+(29, 0, 'VTP-29', '(20,717)', 1),
+(28, 0, 'VTP-28', '(824,620)', 1),
+(27, 0, 'VPMA-034-3', '(556,620)', 1),
+(26, 0, 'VEMAH-341', '(288,620)', 1),
+(7, 0, 'VLM-411', '(824,373)', 1),
+(25, 0, 'VTUTP-355', '(20,620)', 1),
+(24, 0, 'CCGC-311', '(288,225)', 1),
+(10, 0, 'VSAV-432', '(288,373)', 1),
+(11, 0, 'VSAV-431', '(20,373)', 1),
+(23, 0, 'FPTL-GFOR', '(556,225)', 1),
+(22, 0, 'EPA-325', '(20,225)', 1),
+(21, 0, 'CCF-241', '(824,128)', 1),
+(18, 0, 'FPTL-111', '(20,128)', 1),
+(19, 0, 'FPTSR-121', '(288,128)', 1),
+(20, 0, 'CCF-231', '(556,128)', 1),
+(15, 0, 'VSAV-433', '(556,373)', 1),
+(16, 0, 'VSAV-142R', '(288,470)', 1),
+(17, 0, 'AR-1', '(20,470)', 1),
+(31, 0, 'VLHR-104', '(288,864)', 1),
+(32, 0, 'VPC-034-2', '(556,864)', 1),
+(33, 0, 'VLCG-100', '(824,864)', 1),
+(34, 0, 'VL-59', '(20,961)', 1),
+(35, 0, 'VL-36', '(288,961)', 1),
+(36, 0, 'VL-78', '(556,961)', 1),
+(37, 0, 'VLTT-89', '(824,961)', 1),
+(38, 0, 'VLTT-56', '(20,1055)', 1);
 
 -- --------------------------------------------------------
 
@@ -157,117 +185,15 @@ INSERT INTO `vhl` (`id`, `statut`, `véhicule`, `cord`, `syno`) VALUES
 -- Structure de la table `wh`
 --
 
-CREATE TABLE `wh` (
-  `id` mediumint NOT NULL,
-  `alias` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `link` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `lastuse` bigint NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `bip`
---
-ALTER TABLE `bip`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `House`
---
-ALTER TABLE `House`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `profil`
---
-ALTER TABLE `profil`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `service`
---
-ALTER TABLE `service`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `tph`
---
-ALTER TABLE `tph`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `tph_frequency`
---
-ALTER TABLE `tph_frequency`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `vhl`
---
-ALTER TABLE `vhl`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `wh`
---
-ALTER TABLE `wh`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `bip`
---
-ALTER TABLE `bip`
-  MODIFY `id` mediumint NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `House`
---
-ALTER TABLE `House`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `profil`
---
-ALTER TABLE `profil`
-  MODIFY `id` mediumint NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `service`
---
-ALTER TABLE `service`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `tph`
---
-ALTER TABLE `tph`
-  MODIFY `id` mediumint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=180;
-
---
--- AUTO_INCREMENT pour la table `tph_frequency`
---
-ALTER TABLE `tph_frequency`
-  MODIFY `id` mediumint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT pour la table `vhl`
---
-ALTER TABLE `vhl`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT pour la table `wh`
---
-ALTER TABLE `wh`
-  MODIFY `id` mediumint NOT NULL AUTO_INCREMENT;
+DROP TABLE IF EXISTS `wh`;
+CREATE TABLE IF NOT EXISTS `wh` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `alias` tinytext COLLATE utf8mb4_bin NOT NULL,
+  `name` text COLLATE utf8mb4_bin NOT NULL,
+  `link` text COLLATE utf8mb4_bin NOT NULL,
+  `lastuse` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/db/files/vhl.py
+++ src/db/files/vhl.py
@@ -1,100 +1,311 @@
 required = {
-    "VSAV-431":{
+    "VSAV":{
         "hdr":2,
+        "ade":0,
         "soff":1,
         "off":0,
         "inf":0,
-        "med":0,   
+        "med":0           
     },
 
-    "VSAV-432":{
-        "hdr":2,
-        "soff":1,
-        "off":0,
-        "inf":0,
-        "med":0,   
-    },
-
-    "VSAV-433":{
-        "hdr":2,
-        "soff":1,
-        "off":0,
-        "inf":0,
-        "med":0,   
-    },
-
-    "VSAV-142R":{
-        "hdr":2,
-        "soff":1,
-        "off":0,
-        "inf":0,
-        "med":0,   
-    },
-
-    "VLM-411":{
+    "VLM":{
         "hdr":0,
+        "ade":0,
         "soff":0,
         "off":0,
         "inf":0,
-        "med":1,   
+        "med":1  
     },
 
-    "AR-1":{
+    "AR":{
         "hdr":0,
+        "ade":1, 
         "soff":0,
         "off":0,
-        "inf":2,
-        "med":1, 
+        "inf":1,
+        "med":1
+    },
+    
+    "VLCG":{
+        "hdr":0,
+        "ade":0, 
+        "soff":0,
+        "off":1,
+        "inf":0,
+        "med":0
+    },
+
+    "VTP":{
+        "hdr":1,
+        "ade":0, 
+        "soff":0,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+
+    "VL":{
+        "hdr":1,
+        "ade":0, 
+        "soff":0,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+
+    "VLHR":{
+        "hdr":1,
+        "ade":0, 
+        "soff":0,
+        "off":1,
+        "inf":0,
+        "med":0
+    },
+
+    "VLTT":{
+        "hdr":1,
+        "ade":0, 
+        "soff":0,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+
+    "VEMAH":{
+        "hdr":2,
+        "ade":0, 
+        "soff":2,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+
+    "VTUTP":{
+        "hdr":1,
+        "ade":0, 
+        "soff":1,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+
+    "CCGC":{
+        "hdr":1,
+        "ade":0, 
+        "soff":1,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+    
+    "FPTL":{
+        "hdr":2,
+        "ade":0, 
+        "soff":2,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+
+    "FPTSR":{
+        "hdr":3,
+        "ade":0, 
+        "soff":2,
+        "off":1,
+        "inf":0,
+        "med":0
+    },
+
+    "EPA":{
+        "hdr":1,
+        "ade":0, 
+        "soff":1,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+    
+    "CCF":{
+        "hdr":2,
+        "ade":0, 
+        "soff":2,
+        "off":0,
+        "inf":0,
+        "med":0
+    },
+
+    "VPC":{
+        "hdr":1,
+        "ade":0, 
+        "soff":0,
+        "off":2,
+        "inf":0,
+        "med":0
+    },
+
+    "VPMA":{
+        "hdr":1,
+        "ade":1, 
+        "soff":1,
+        "off":0,
+        "inf":1,
+        "med":1
     }
 }
 
 calculation = {
-   "VSAV-431":{
+   "VSAV":{
         "hdr":["hdr"],
+        "ade":[""], 
         "soff":["soff", "inf"],
         "off":["off", "med"],
         "inf":[""],
-        "med":[""],   
+        "med":[""]
     },
 
-    "VSAV-432":{
+    "VLM":{
         "hdr":["hdr"],
-        "soff":["soff", "inf"],
-        "off":["off", "med"],
-        "inf":[""],
-        "med":[""],   
-    },
-
-    "VSAV-433":{
-        "hdr":["hdr"],
-        "soff":["soff", "inf"],
-        "off":["off", "med"],
-        "inf":[""],
-        "med":[""],   
-    },
-
-    "VSAV-142R":{
-        "hdr":["hdr"],
-        "soff":["soff", "inf"],
-        "off":["off", "med"],
-        "inf":[""],
-        "med":[""],   
-    },
-
-    "VLM-411":{
-        "hdr":["hdr"],
+        "ade":[""],
         "soff":["soff"],
         "off":["off"],
         "inf":["inf"],
-        "med":["med"],   
+        "med":["med"]
     },
 
-    "AR-1":{
+    "AR":{
         "hdr":["hdr"],
+        "ade":["ade"],
         "soff":["soff"],
         "off":["off"],
         "inf":["inf"],
-        "med":["med"],   
+        "med":["med"]   
+    },
+
+    "VPMA":{
+        "hdr":["hdr"],
+        "ade":["ade"],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":["inf"],
+        "med":["med"]   
+    },
+
+    "VLCG":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "VTP":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "VL":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "VLTT":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "VLHR":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "VEMAH":{
+        "hdr":["hdr"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "VTUTP":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "CCGC":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "FPTL":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "FPTSR":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "EPA":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "CCF":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
+    },
+
+    "VPC":{
+        "hdr":["hdr", "inf", "med"],
+        "ade":[""],
+        "soff":["soff"],
+        "off":["off"],
+        "inf":[""],
+        "med":[""]   
     }
+    
 }
 
